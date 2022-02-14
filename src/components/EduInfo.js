d72035760c0a82a9cf13aa87d4ab057acd32e64a
MissLiner <miss.liner@yahoo.com>
2022-02-14 09:51:13 -0500

    Add basic components

---
+++ src/components/EduInfo.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react';
+import App from "App";
+
+class GenInfo extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+
+    )
+  }
+}
