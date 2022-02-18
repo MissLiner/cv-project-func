9e17f96b741ad59379c1a6a9a2dbb01be7cde503
MissLiner <miss.liner@yahoo.com>
2022-02-18 14:15:09 -0500

    Fix bug not clearing form with handleAdd

--- src/components/InputDisplay.js
+++ src/components/InputDisplay.js
@@ -29,30 +29,9 @@ class InputDisplay extends Component {
         })
       )
     }
-    // const renderAllDivs = () => {
-    //   let keyID = 100;
-    //   details.map(detail => {
-    //     const detailDiv = document.createElement('div');
-    //     Object.entries(detail).forEach(entry => {
-    //       keyID += 1;
-    //       const entryDiv = <Div 
-    //                         key={keyID}
-    //                         divText={entry[1]} /> 
-    //       detailDiv.appendChild(entryDiv);
-    //     })
-    //     Object.entries(detail).map(entry => {
-
-    //       return(
-
-    //           )
-    //         })
-    //       )
-    //     })
-    //   )
-    // }
     const addAddBtn = () => {
       if(needsAddBtn === true) {
-        return <Button btnText="Add New" clickFunc={addFunc} section={sectionKey}></Button>
+        return <Button btnText="Add New" clickFunc={addFunc} sectionKey={sectionKey}></Button>
       }
     }
 
