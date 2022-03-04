56b07fb7a157396ec3245f608379eae246c2961c
MissLiner <miss.liner@yahoo.com>
2022-03-04 10:38:18 -0500

    Retrieve genInfo from localStorage on mount

--- src/components/DisplayGenInfo.js
+++ src/components/DisplayGenInfo.js
@@ -31,10 +31,11 @@ class DisplayGenInfo extends Component {
     this.handleChange = handleChange.bind(this);
     this.handleSubmit = handleSubmit.bind(this);
     this.handleEdit = handleEdit.bind(this);
-
-    const isLocalStorage = localStorage.getItem('genInfo') === 'true';
-    const storedObj = isLocalStorage ? JSON.parse(window.localStorage.getItem('genInfo')) : '';
-    this.setState({ storedObj });
+  }
+  componentDidMount() {
+    const isLocalStorage = localStorage.getItem('genInfo');
+    const genInfo = isLocalStorage ? JSON.parse(isLocalStorage) : '';
+    this.setState({ genInfo });
   }
   
   render() {
